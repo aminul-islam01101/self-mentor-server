e10374b35080777df60b8126d3e0a8e3cf9750b9
aminulislam01101 <aminul.islam01101@gmail.com>
2022-10-25 18:27:49 +0600

    some fake data added

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const category = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-  res.send('server Running update');
+  res.send('server Running update json');
 });
 
-app.get('/categories', (req, res) => {
-  res.send(categories);
+app.get('/category', (req, res) => {
+  res.send(category);
 });
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === '08') {
+      res.send(courses);
+  }
+  else {
+      const course_category = courses.filter(n => n.category_id === id);
+      res.send(course_category);
+  }
+})
+app.get('/course/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find(n => n._id === id);
+  res.send(selectedCourse);
+});
+
 
 app.listen(port, () => {
   console.log('Server running on port', port);
