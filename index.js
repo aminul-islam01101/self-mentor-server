c6e9df301db9bf05db5c788b7d73cdcf7b923b94
aminulislam01101 <aminul.islam01101@gmail.com>
2022-10-26 19:46:01 +0600

    all data category updated

--- index.js
+++ index.js
@@ -11,26 +11,27 @@ const courses = require('./data/courses.json');
 app.get('/', (req, res) => {
   res.send('server Running update json');
 });
-
+// category name data
 app.get('/category', (req, res) => {
   res.send(category);
 });
+// according to category data
 app.get('/category/:id', (req, res) => {
   const id = req.params.id;
-  if (id === '08') {
-      res.send(courses);
-  }
-  else {
-      const course_category = courses.filter(n => n.category_id === id);
-      res.send(course_category);
-  }
-})
+
+  const course_category = courses.filter((n) => n.category_id === id);
+  res.send(course_category);
+});
+// all data
+app.get('/courses', (req, res) => {
+  res.send(courses);
+});
+// individual course data
 app.get('/course/:id', (req, res) => {
   const id = req.params.id;
-  const selectedCourse = courses.find(n => n._id === id);
+  const selectedCourse = courses.find((n) => n._id === id);
   res.send(selectedCourse);
 });
-
 
 app.listen(port, () => {
   console.log('Server running on port', port);
